17dd6c333836dad0bd5710855e1c95f30bbca315
Sumil Verma <verma.sumil257@gmail.com>
2025-04-14 11:19:36 +0530

    resturcture directory | server start setup

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { PORT, NODE_ENV } from './config';
+const app = express();
+app.disable('x-powered-by');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
