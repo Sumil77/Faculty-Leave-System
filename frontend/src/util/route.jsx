236a5f013b0e29f3207a5df19669f2a616e54677
Sumil Verma <verma.sumil257@gmail.com>
2025-04-17 17:49:13 +0530

    fixed session logic

--- frontend/src/util/route.jsx
+++ frontend/src/util/route.jsx
@@ -22,11 +22,7 @@ export const ProtectedRoute = ({ children }) => {
   const loggedIn = useSelector((state) => Boolean(state.session.userId));
   const dispatch = useDispatch();
   const [checking, setChecking] = useState(true);
-
-  const checkInterval = 10 * 1000; // 5 minutes
-  const idleTimeout = 20 * 1000; // 10 minutes of inactivity
-
-  let idleTimer;
+  const location = useLocation();
 
   const validateSession = async () => {
     console.log(isBypassAuth);
@@ -56,32 +52,12 @@ export const ProtectedRoute = ({ children }) => {
     }
   };
 
-  // Reset idle timer on mouse click (or any other event like key press, scroll, etc.)
-  const resetIdleTimer = () => {
-    clearTimeout(idleTimer);
-    idleTimer = setTimeout(() => {
-      validateSession(); // Trigger session check after inactivity timeout
-    }, idleTimeout);
-  };
-
   useEffect(() => {
     // Initial session check
+    setChecking(true);
     validateSession();
 
-    // Set interval to check session every checkInterval time
-    const intervalId = setInterval(() => {
-      validateSession();
-    }, checkInterval);
-
-    // Set event listener for mouse clicks
-    window.addEventListener("click", resetIdleTimer);
-
-    // Cleanup
-    return () => {
-      clearInterval(intervalId);
-      window.removeEventListener("click", resetIdleTimer); // Remove event listener on cleanup
-    };
-  }, [dispatch]);
+  }, [location.pathname,dispatch]);
 
   if (checking) return <div>Loading...</div>;
 
